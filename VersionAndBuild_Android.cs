efb281392c675c1b256d224c1d66975fbead86cd
umairjaved932 <35454474+umairjaved932@users.noreply.github.com>
2020-07-09 05:06:43 +0500

    Add files via upload

---
+++ VersionAndBuild_Android.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using LEDiBond.Interfaces;
+using Xamarin.Forms;
+using LEDiBond.Droid;
+
+[assembly: Dependency(typeof(VersionAndBuild_Android))]
+
+namespace LEDiBond.Droid
+{
+    public class VersionAndBuild_Android : IAppVersionAndBuild
+    {
+        PackageInfo _appInfo;
+        public VersionAndBuild_Android()
+        {
+            var context = Android.App.Application.Context;
+            _appInfo = context.PackageManager.GetPackageInfo(context.PackageName, 0);
+        }
+        public string GetVersionNumber()
+        {
+            return _appInfo.VersionName.ToString();
+        }
+        public string GetBuildNumber()
+        {
+            return _appInfo.VersionCode.ToString();
+        }
+    }
+}
