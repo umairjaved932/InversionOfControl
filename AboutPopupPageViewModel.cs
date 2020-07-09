3f33b9d4ee4bd0d9638738d8186e52fc516cb320
umairjaved932 <35454474+umairjaved932@users.noreply.github.com>
2020-07-09 05:06:22 +0500

    Add files via upload

---
+++ AboutPopupPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using LEDiBond.Interfaces;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace LEDiBond.ViewModels
+{
+    public class AboutPopupPageViewModel : ViewModelBase
+    {
+
+        string _versionNumber;
+        public string VersionNumber
+        {
+            get { return _versionNumber; }
+            set { SetProperty(ref _versionNumber, value); }
+        }
+
+        public AboutPopupPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            VersionNumber = "Version " + AppInfo.VersionString;
+        }
+    }
+}
