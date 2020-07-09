01c984f9bf4b239d4f535f50a652dceb04ddd6fb
umairjaved932 <35454474+umairjaved932@users.noreply.github.com>
2020-07-09 05:03:08 +0500

    Add files via upload

---
+++ Interfaces/IAppVersionAndBuild.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace LEDiBond.Interfaces
+{
+    public class IAppVersionAndBuild
+    {
+        public string GetVersionNumber()
+        {
+            return typeof(App).GetTypeInfo().Assembly.GetName().Version.ToString();
+        }
+        public string GetBuildNumber()
+        {
+            return typeof(App).GetTypeInfo().Assembly.GetName().Version.Build.ToString();
+        }
+    }
+}
